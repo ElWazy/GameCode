4a50357988a70c0ae3dadd523f7e140bdceb1058
ElWazy <ElCazero.Fer@gmail.com>
2021-10-25 16:37:25 -0300

    (Design) Insumos/GiftCards Screen

---
+++ app/src/main/java/com/lutrias/projectunit2_matiasmarchant_santiagofierro/InsumosActivity.java
@@ -0,0 +1,13 @@
+package com.lutrias.projectunit2_matiasmarchant_santiagofierro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsumosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insumos);
+    }
+}
