f2ace4700f327eea9a5bf103ca6cb101c4308178
ElWazy <ElCazero.Fer@gmail.com>
2021-10-25 14:41:55 -0300

    Initial commit

---
+++ app/src/main/java/com/lutrias/projectunit2_matiasmarchant_santiagofierro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lutrias.projectunit2_matiasmarchant_santiagofierro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
