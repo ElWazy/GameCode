56ce38597f9bc5836afcc90a2d29b39c52e4fc6b
Matías Sebastián Marchant Soto <marchantmatias706@gmail.com>
2021-11-13 23:03:17 -0300

    Video-hay que cambiarlo

--- app/src/main/java/com/lutrias/projectunit2_matiasmarchant_santiagofierro/HomeActivity.java
+++ app/src/main/java/com/lutrias/projectunit2_matiasmarchant_santiagofierro/HomeActivity.java
@@ -3,9 +3,11 @@ package com.lutrias.projectunit2_matiasmarchant_santiagofierro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.lutrias.projectunit2_matiasmarchant_santiagofierro.objects.Card;
@@ -24,7 +26,20 @@ public class HomeActivity extends AppCompatActivity {
     }
     private void loadComponents(){
         buttoncard = findViewById(R.id.btnCard);
+        videoView      = findViewById(R.id.video);
+
+        String ruta = "android.resource://"+getPackageName()+ "/"+ R.raw.video;
+        Uri uri = Uri.parse(ruta);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        //CONTROLES
+        /*
+        MediaController media = new MediaController(this);
+        videoView.setMediaController(media);
         card = new Card();
+
+         */
     }
 
 
